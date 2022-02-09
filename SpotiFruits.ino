86194d6abeef84df4599e2d5650d247f55d7cb7c
alexisg09 <54796824+alexisg09@users.noreply.github.com>
2022-02-09 14:20:24 +0100

    Update SpotiFruits.ino

--- SpotiFruits.ino
+++ SpotiFruits.ino
@@ -1,8 +1,8 @@
 #include <BLEMIDI_Transport.h>
 #include <hardware/BLEMIDI_ESP32.h>
+#include <MIDI.h>
 
 BLEMIDI_CREATE_INSTANCE("SpotiFruits",MIDI);  
-int tempo = 280;
 
 const int NUMBER_OF_TOUCH_PIN = 9;
 const int touchPin[NUMBER_OF_TOUCH_PIN] = {4,2,15,13,12,14,27,33,32}; 
@@ -11,38 +11,86 @@ int touchValue[NUMBER_OF_TOUCH_PIN];
 
 const int threshold = 20;
 
+
+
+ //MIDI_CREATE_DEFAULT_INSTANCE();
+
+ //Please name your piezo.
+ //The piezo named snare is connected to the A0 pin
+
+
+
+
+
+
 void setup(){
   Serial.begin(9600);
   MIDI.begin(10);
+  
+  
 }
 void loop(){
-  for(int i = 0; i < NUMBER_OF_TOUCH_PIN; i++){
+  for(int i = 0; i < NUMBER_OF_TOUCH_PIN; i++){ 
     touchValue[i] = touchRead(touchPin[i]);
     if(touchValue[i] < threshold){
       if(touchPinUnlock[i] == true){
-    
+
+     
+
+     
+       /***  
         MIDI.sendNoteOn(36, 100, 10);
         MIDI.sendNoteOff(36, 0, 10);
         MIDI.sendNoteOn(44, 100, 10);
         MIDI.sendNoteOff(44, 0, 10);
-        delay(tempo);
-    
-        MIDI.sendNoteOn(44, 100, 10);
-        MIDI.sendNoteOff(44, 0, 10);
-        delay(tempo);
-    
-        MIDI.sendNoteOn(38, 100, 10);
-        MIDI.sendNoteOff(38, 0, 10);
-        MIDI.sendNoteOn(44, 100, 10);
-        MIDI.sendNoteOff(44, 0, 10);
-        delay(tempo);
-    
-        MIDI.sendNoteOn(44, 100, 10);
-        MIDI.sendNoteOff(44, 0, 10);
-        delay(tempo);
+        ***/
 
         Serial.print("detect on PIN ");
         Serial.println(touchPin[i]);
+
+        switch(touchPin[i]) {
+          case 14:
+        MIDI.sendNoteOn(39, 70, 10);
+       MIDI.sendNoteOff(39, 0, 10);
+        // delay(100);
+
+
+          break;
+           case 15: 
+           MIDI.sendNoteOn(37, 100, 10);
+           MIDI.sendNoteOff(37, 0, 10);
+          //   delay(100);
+
+
+           break;
+          case 33: 
+           MIDI.sendNoteOn(36, 100, 10);
+           MIDI.sendNoteOff(36, 0, 10);
+           //  delay(100);
+
+
+           break;
+
+           
+          break;
+           case 4: 
+           MIDI.sendNoteOn(38, 100, 10);
+           MIDI.sendNoteOff(38, 0, 10);
+           //  delay(100);
+
+
+           break;
+          case 27: 
+           MIDI.sendNoteOn(40, 100, 10);
+           MIDI.sendNoteOff(40, 0, 10);
+           //  delay(100);
+
+
+           break;
+           
+           default: 
+           break;
+        }
       }
       else
       {
@@ -54,5 +102,3 @@ void loop(){
     }
   }
 }
-
- 
